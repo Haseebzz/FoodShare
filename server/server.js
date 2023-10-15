fae6e5347fc837aed628461a70a49e07b5ec86e4
Haseebzz <chaudhuryhaseeb35@gmail.com>
2023-10-14 20:55:20 -0400

    created server.js file

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import cors from 'cors'
+import recipeRoutes from "./routes/RecipeRoutes.js"
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-
+app.use('/recipes', recipeRoutes)
 app.get('/', (req, res) => {
-  res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">ON THE FLY</h1>')
+  res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">FoodShare</h1>')
 })
 
 const PORT = process.env.PORT || 3001
