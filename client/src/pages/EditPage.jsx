e53b15330528c1f1947f15052b1c08b4174a02f7
Haseeb Chaudhury <98563266+Haseebzz@users.noreply.github.com>
2023-11-16 21:38:10 -0500

    Update EditPage.jsx

--- client/src/pages/EditPage.jsx
+++ client/src/pages/EditPage.jsx
@@ -28,7 +28,7 @@ const EditPage = () => {
     // Fetch the recipe data for editing
     const fetchRecipeForEdit = async () => {
       try {
-        const response = await fetch(`http://localhost:3001/recipes/${id}`);
+        const response = await fetch(`https://foodshare-f5xd0ahh2-haseebzz.vercel.app/recipes/${id}`);
         if (response.ok) {
           const data = await response.json();
           setRecipe(data);
