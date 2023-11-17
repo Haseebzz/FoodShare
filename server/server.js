eb2e2b5b75b7650e4e902f42abec29faea78648d
Haseeb Chaudhury <98563266+Haseebzz@users.noreply.github.com>
2023-11-16 19:22:33 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from 'cors'
 import recipeRoutes from "./routes/RecipeRoutes.js"
 import userRoutes from "./routes/userRoutes.js"
 import commentRoutes from './routes/commentRoutes.js'
-
+import collectionRoute from './routes/CollectionsRoutes.js'
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -11,6 +11,7 @@ app.use(cors())
 app.use('/recipes', recipeRoutes)
 app.use('/auth', userRoutes)
 app.use('/comments', commentRoutes)
+app.use('/collections', collectionRoute)
 app.get('/', (req, res) => {
   res.status(200).send('<h1 style="text-align: center; margin-top: 50px;">FoodShare</h1>')
 })
